883f70d0e8568511809d029d0e4ee68db54ee708
Narwrynn <Rigellashley@gmail.com>
2016-08-07 14:48:02 -0400

    Update pokedex.js

---
+++ js/pokedex.js
@@ -0,0 +1,33 @@
+
+$( document ).ready(function() {
+    
+ 
+    var currentPokemon;
+    var html = '';
+    
+    
+    for(var prop in PokemonScript.Pokemon) 
+    {       
+            currentPokemon = prop;
+            html +=
+            '<tr data-toggle="modal" data-target="#PokemonModal" value="' + currentPokemon + '"><td>'
+            +Pokemon[prop].num+
+            '</td><td>'
+            +Pokemon[prop].species+
+            '</td><td>'
+            +Pokemon[prop].types[0];
+            if  (Pokemon[prop].types.length == 2) {
+            html +=
+            '<br />' +Pokemon[prop].types[1];}
+            html+=
+            '</td></tr>';
+            
+     }
+$('#Pokedex tr').first().after(html);
+   
+   
+
+   
+});
+
+
