39cd74a6123c53469adf511b98f91eab322a08b4
Narwrynn <Rigellashley@gmail.com>
2016-08-07 14:46:14 -0400

    Update PokedexScript.js

--- js/PokedexScript.js
+++ js/PokedexScript.js
@@ -836,39 +836,3 @@ volcanion:{num:721,species:"Volcanion",types:["Fire","Water"],gender:"N",baseSta
 
 };
 
-
-$( document ).ready(function() {
-    
-    
- $( "tr" ).click(function() {
-  alert( "Handler for .click() called." );});
-    
-    var currentPokemon;
-    var html = '';
-    
-    
-    for(var prop in Pokemon) 
-    {       
-            currentPokemon = prop;
-            html +=
-            '<tr data-toggle="modal" data-target="#PokemonModal" value="' + currentPokemon + '"><td>'
-            +Pokemon[prop].num+
-            '</td><td>'
-            +Pokemon[prop].species+
-            '</td><td>'
-            +Pokemon[prop].types[0];
-            if  (Pokemon[prop].types.length == 2) {
-            html +=
-            '<br />' +Pokemon[prop].types[1];}
-            html+=
-            '</td></tr>';
-            
-     }
-$('#Pokedex tr').first().after(html);
-   
-   
-
-   
-});
-
-
