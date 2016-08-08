b3c0696e95af60dbc10a4de57211fd00648ea52e
Narwrynn <Rigellashley@gmail.com>
2016-08-08 18:02:53 -0400

    Update pokedex.js

--- js/pokedex.js
+++ js/pokedex.js
@@ -36,13 +36,13 @@ $(".table").on('click','tr',function(e){
    $( "#Pokedex tr" ).click(function() {
        $("#PokedexModalContent").append("
        
-        <div class="modal-header"><h3>" + pokemonRow + "</h3>
+        <div class='modal-header'><h3>" + pokemonRow + "</h3>
                 </div>
-                <div class="modal-body">
+                <div class='modal-body'>
                 <p>Testing Text</p>
                 </div>
-                <div class="modal-footer">
-                <a class="btn btn-default" data-dismiss ="modal">
+                <div class='modal-footer'>
+                <a class='btn btn-default' data-dismiss ='modal'>
                                 Close
                 </a>
                 </div>
