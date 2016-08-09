003d4d9a4931d6e385065bca8a29f8ced78478d7
Narwrynn <Rigellashley@gmail.com>
2016-08-08 20:03:15 -0400

    Update pokedex.js

--- js/pokedex.js
+++ js/pokedex.js
@@ -33,10 +33,8 @@ $("#Pokedex").on('click','tr',function(e){
     pokemonRow = $(this).attr('value');
      
        html2 = "<div class='modal-dialog' id='PokedexModalDialog'><div class='modal-content'><div class='modal-header'><a href='#'><h3>" 
-    + Pokemon[pokemonRow].species + 
-       "</h3></a></div><div class='modal-body'><h7>Abilities:</h7>"
-    + for(x=0; x < Pokemon[pokemonRow].abilities.length; x++ ){html2+="<p> " + Pokemon[pokemonRow].abilities.[x] + " </p>";} +
-       "<table class='table-striped'><tr><th> HP </th><th> Attack </th><th> Defence </th><th> Special <br /> Attack </th><th> Special <br /> Defence </th><th> Speed </th></tr><tr><td>"
+       + Pokemon[pokemonRow].species + 
+       "</h3></a></div><div class='modal-body'><h7>Abilities:</h7><table class='table-striped'><tr><th> HP </th><th> Attack </th><th> Defence </th><th> Special <br /> Attack </th><th> Special <br /> Defence </th><th> Speed </th></tr><tr><td>"
 	+Pokemon[pokemonRow].baseStats.hp+
 	"</td><td>"
 	+Pokemon[pokemonRow].baseStats.atk+
@@ -49,9 +47,9 @@ $("#Pokedex").on('click','tr',function(e){
 	"</td><td>"
 	+Pokemon[pokemonRow].baseStats.spe+
 	"</td></tr></table></div><div class='modal-footer'><a class='btn btn-default' data-dismiss ='modal'>Close</a></div>");
-	$("#PokemonModal").append(html2);
-                
+	$("#PokemonModal").append(html2)
 });
+		
                 
                 $(document).on('hide.bs.modal','#PokemonModal', function () {
                 $("#PokedexModalDialog").remove();
