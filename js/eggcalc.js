f69b1b6346d9be73c6e0fd0665c0c403cddc09bb
Narwrynn <Rigellashley@gmail.com>
2016-08-11 12:32:36 -0400

    Create eggcalc.js

---
+++ js/eggcalc.js
@@ -0,0 +1,7 @@
+  function validate() {
+        if (document.getElementById('remember').checked) {
+            alert("checked");
+        } else {
+            alert("You didn't check it! Let me check it for you.");
+        }
+    }
